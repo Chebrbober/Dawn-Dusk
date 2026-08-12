b46ffb2b82fa348f80dc6a272a28654ee4555b8a
beaver <meyar2223@gmail.com>
2026-08-12 15:22:35 +0300

    add neat visuals, add tile map

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,22 @@
 class_name Player extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var move_component: MoveComponent = $MoveComponent
+
 func _init() -> void:
     GameManager.player = self
+
+func _process(delta: float) -> void:
+    animate_the_ship()
+
+func animate_the_ship() -> void:
+    if move_component.velocity.x < 0:
+        animated_sprite_2d.play("left")
+    elif move_component.velocity.x > 0:
+        animated_sprite_2d.play("right")
+    elif move_component.velocity.y > 0:
+        animated_sprite_2d.play("down")
+    elif move_component.velocity.y < 0:
+        animated_sprite_2d.play("up")
+    else:
+        animated_sprite_2d.play("down")
