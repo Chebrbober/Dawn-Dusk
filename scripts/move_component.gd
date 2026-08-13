bc156d64c93f5cd755c6ce1bf18b6b8a056ee941
beaver <meyar2223@gmail.com>
2026-08-13 11:28:42 +0300

    add acceleration logic (independent from fps) to the move component

--- scripts/move_component.gd
+++ scripts/move_component.gd
@@ -2,15 +2,18 @@ class_name MoveComponent extends Node
 
 @export var actor: Node2D
 @export var velocity: Vector2
-@export var accel_time: float = 0.1
-@export var tween_trans_type: Tween.TransitionType = Tween.TransitionType.TRANS_QUAD
-@export var tween_ease_type: Tween.EaseType = Tween.EaseType.EASE_IN_OUT
 @export var speed: float = 100.0
+@export var accel: float = 25.0 # higher = faster approach to target 
 
 var _target_velocity: Vector2 = Vector2.ZERO
-var _velocity_tween: Tween = null
 
 func _physics_process(delta: float) -> void:
+	if not actor:
+		return
+
+	var alpha = 1.0 - exp(-accel * delta)
+	velocity = velocity.lerp(_target_velocity, alpha)
+
 	actor.translate(velocity * delta)
 	actor.move_and_slide()
 
@@ -18,10 +21,4 @@ func set_target_dir(dir: Vector2) -> void:
 	var new_velocity = dir * speed
 	if new_velocity == _target_velocity:
 		return
-
 	_target_velocity = new_velocity
-	if _velocity_tween:
-		_velocity_tween.kill()
-
-	_velocity_tween = create_tween().set_trans(tween_trans_type).set_ease(tween_ease_type)
-	_velocity_tween.tween_property(self, "velocity", _target_velocity, accel_time)
